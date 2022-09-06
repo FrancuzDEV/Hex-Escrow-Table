85cfe5e69fb4df8faaf7e28b29064b140b2930f5
Francuz <90420828+FrancuzDEV@users.noreply.github.com>
2022-09-06 21:03:13 +0200

    Main file

---
+++ main.lua
@@ -0,0 +1,24 @@
+function string.fromhex(str)
+    return (str:gsub('..', function (cc)
+        return string.char(tonumber(cc, 16))
+    end))
+end
+
+function string.tohex(str)
+    return (str:gsub('.', function (c)
+        return string.format('%02X', string.byte(c))
+    end))
+end
+
+function hex_dump(buf)
+      for i=1,math.ceil(#buf/16) * 16 do
+         if (i-1) % 16 == 0 then io.write(string.format('%08X  ', i-1)) end
+         io.write( i > #buf and '   ' or string.format('%02X ', buf:byte(i)) )
+         if i %  8 == 0 then io.write(' ') end
+         if i % 16 == 0 then io.write( buf:sub(i-16+1, i):gsub('%c','.'), '\n' ) end
+      end
+   end
+local tab = [[
+	-- Enter there your hex
+]]
+hex_dump(tab)
